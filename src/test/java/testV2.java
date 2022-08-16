856d8bcb03880f4313434f1a47e3025e3b56fe56
Alissa-1 <alena.cheremukhina@gmail.com>
2022-08-16 17:47:44 +0300

    tests v2, v3 added and failed 'cause wrong body-data

--- src/test/java/testV2.java
+++ src/test/java/testV2.java
@@ -1,24 +1,27 @@
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
-public class testV1 {
+public class testV2 {
     @Test
-    void testPostRequest() {
+    void testPostQuantityOfBooks() {
         // Given - When - Then
 // Предусловия
+        File file = new File("./src/test/resources/myJson.json");
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .header("Content-Type", "application/json")
+                .body(file) // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
 // Проверки
                 .then()
-                .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data.store.book", hasSize(41))
         ;
     }
 }
---
+++ src/test/java/testV3.java
@@ -0,0 +1,27 @@
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class testV3 {
+    @Test
+    void testPostLastAuthor() {
+        // Given - When - Then
+// Предусловия
+        File file = new File("./src/test/resources/myJson.json");
+        given()
+                .baseUri("https://postman-echo.com")
+                .header("Content-Type", "application/json")
+                .body(file) // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .body("data.store.book.author.last()", equalTo("J. R. R. Tolkieng"))
+        ;
+    }
+}
